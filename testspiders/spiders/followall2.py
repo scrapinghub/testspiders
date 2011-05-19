c530466248ad4e0f2fd50ea3bb4af345ce63a2a5
Rolando Espinoza La fuente <darkrho@gmail.com>
2011-05-19 12:24:02 -0400

    added doc for followall2 spider and parse only html responses

--- testspiders/spiders/followall2.py
+++ testspiders/spiders/followall2.py
@@ -1,4 +1,8 @@
+"""
+Crawl-all spider, stores headers and performs basic parsing for every html page
+"""
 from scrapy.item import Field
+from scrapy.http import HtmlResponse
 from scrapy.selector import HtmlXPathSelector
 
 from testspiders.items import Page
@@ -21,13 +25,14 @@ class FollowAll2Spider(FollowAllSpider):
 
     def _get_item(self, response):
         item = super(FollowAll2Spider, self)._get_item(response)
-        hxs = HtmlXPathSelector(response)
-        try:
-            item['title'] = hxs.select('//title[1]/text()').extract()[0]
-        except IndexError:
-            pass
-        item['dom_nodes'] = hxs.select('count(//*)').extract()[0]
-        item['dom_tables'] = hxs.select('count(//table)').extract()[0]
-        item['scripts'] = hxs.select('//script/@src').extract()
-        item['headers'] = response.headers.copy()
+        if isinstance(response, HtmlResponse):
+            hxs = HtmlXPathSelector(response)
+            try:
+                item['title'] = hxs.select('//title[1]/text()').extract()[0]
+            except IndexError:
+                pass
+            item['dom_nodes'] = hxs.select('count(//*)').extract()[0]
+            item['dom_tables'] = hxs.select('count(//table)').extract()[0]
+            item['scripts'] = hxs.select('//script/@src').extract()
+            item['headers'] = response.headers.copy()
         return item
