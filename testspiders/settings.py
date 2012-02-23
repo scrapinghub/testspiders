cdb48e25785f91eee7fcde2a8b86a4b034e4107c
Pablo Hoffman <pablo@pablohoffman.com>
2012-02-23 10:23:57 -0200

    removed boilerplate comments

--- testspiders/settings.py
+++ testspiders/settings.py
@@ -1,11 +1,3 @@
-# Scrapy settings for testspiders project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/topics/settings.html
-#
-
 BOT_NAME = 'testspiders'
 BOT_VERSION = '1.0'
 
